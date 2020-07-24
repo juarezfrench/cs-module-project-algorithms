cbb627e501aaa73c6ab794a8e74891fa8b63c999
juarezfrench <markedfrench@gmail.com>
2020-07-24 10:38:27 -0500

    Finished eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+
     # Your code here
 
-    pass
+def eating_cookies(n, cache={}):
+
+    if n < 2:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    elif n in cache:
+        return cache[n]
+    else:
+        # find all paths to 0 cookies
+        one_cookie_eaten = eating_cookies(n - 1, cache)
+        two_cookies_eaten = eating_cookies(n - 2, cache)
+        three_cookies_eaten = eating_cookies(n - 3, cache)
+        cache[n] = one_cookie_eaten + two_cookies_eaten + three_cookies_eaten
+        return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
